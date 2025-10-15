1e1b4b72cb5002e0c6ccb09aa1e3d09479eaa9a1
nguyensuc769 <sucnv@cscmobi.com>
2025-10-15 15:48:19 +0700

    update full

--- assets/scrtips/TypedItem.ts
+++ assets/scrtips/TypedItem.ts
@@ -2,15 +2,6 @@ import { _decorator, Component } from "cc";
 import { AnimalType } from "./AnimalType";
 const { ccclass, property } = _decorator;
 
-/** 5 loại type */
-export enum ItemType {
-  Type1 = 1,
-  Type2 = 2,
-  Type3 = 3,
-  Type4 = 4,
-  Type5 = 5,
-}
-
 /** Component giữ type cho node được spawn */
 @ccclass("TypedItem")
 export class TypedItem extends Component {
